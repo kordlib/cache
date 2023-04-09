b0d2107b5ad6044cd35b9011670a56177f499a4a
Michael Rittmeister <michael@rittmeister.in>
2023-04-09 13:25:19 +0200

    Use published version of Gradle plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,12 +8,11 @@ repositories {
     gradlePluginPortal()
     // until Dokka 1.8.0 is released and we no longer need dev builds, see https://github.com/kordlib/kord/pull/755
     maven("https://maven.pkg.jetbrains.space/kotlin/p/dokka/dev")
-    mavenLocal()
+    maven("https://kord.jfrog.io/artifactory/gradle-dev/")
 }
 
 dependencies {
     implementation(libs.bundles.gradlePlugins)
     implementation(gradleApi())
     implementation(localGroovy())
-    implementation("dev.kord:kotlin-multiplatform-tools:0.0.1")
 }
--- buildSrc/src/main/kotlin/Projects.kt
+++ buildSrc/src/main/kotlin/Projects.kt
@@ -8,7 +8,7 @@ val isJitPack get() = "true" == System.getenv("JITPACK")
 object Library {
     const val name = "cache"
     const val group = "dev.kord.cache"
-    const val version = "0.5.0"
+    const val version = "0.5.x-SNAPSHOT"
     const val description = "Adaptable cache with query-like operations"
 }
 
