f395e9600e6d700425fb3a289a734f9bc59faf2f
Michael Rittmeister <michael@rittmeister.in>
2024-01-26 02:15:42 +0100

    Remove unused import

--- buildSrc/src/main/kotlin/multiplatform-module.gradle.kts
+++ buildSrc/src/main/kotlin/multiplatform-module.gradle.kts
@@ -1,6 +1,5 @@
 import com.vanniktech.maven.publish.JavadocJar
 import com.vanniktech.maven.publish.KotlinMultiplatform
-import org.gradle.kotlin.dsl.support.kotlinCompilerOptions
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.targets.jvm.tasks.KotlinJvmTest
