319b6916ca98e293330241c02fdf440a5d9cf827
Michael Rittmeister <michael@rittmeister.in>
2024-01-29 21:19:25 +0100

    Add explainer for private repo

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 repositories {
     gradlePluginPortal()
     mavenCentral()
+    // Repo providing the Kord Gradle plugin
     maven("https://europe-west3-maven.pkg.dev/mik-music/kord")
 }
 
